8fb48f3c11a87529d57568b7d84348f2d248f242
Felipe Rocha <felipemota.rocha@gmail.com>
2020-03-18 23:49:38 -0300

    Updates

---
+++ precache-manifest.45f4782953db78fe290e45469de16358.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3dddf18f5e82aa277aa9dee0ad7f2da8",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "f2ce5f8aa8fc38c67809",
+    "url": "/monsters-rolodex/static/css/main.bfb76174.chunk.css"
+  },
+  {
+    "revision": "483bbd133c204aa47b97",
+    "url": "/monsters-rolodex/static/js/2.1831c00e.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex/static/js/2.1831c00e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2ce5f8aa8fc38c67809",
+    "url": "/monsters-rolodex/static/js/main.c5b0dd5b.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
